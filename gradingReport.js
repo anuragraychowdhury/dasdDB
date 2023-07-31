7137d08b61114f2a8114d8532800cc666c4ca9ec
anuragraychowdhury <110551628+anuragraychowdhury@users.noreply.github.com>
2023-07-31 10:59:13 -0500

    Update gradingReport.js

--- gradingReport.js
+++ gradingReport.js
@@ -5,7 +5,6 @@ const urlParams = new URLSearchParams(window.location.search);
 const studentName = urlParams.get('student-name');
 const startDate = formatDate(urlParams.get('start-date'));
 const endDate = formatDate(urlParams.get('end-date'));
-console.log("nizz",startDate,endDate);//penis
 const teacher = urlParams.get('teacher');
 const jobCoachName = urlParams.get('job-coach-name');
 const workSite = urlParams.get('work-site');
@@ -20,7 +19,6 @@ document.getElementById('display-teacher').innerText = teacher;
 document.getElementById('display-job-coach-name').innerText = jobCoachName;
 document.getElementById('display-work-site').innerText = workSite;
 document.getElementById('display-school').innerText = school;
-loadReport();
 
 
 function generateReport() {
@@ -42,63 +40,118 @@ function formatDate(dateString) {
   return `${year}-${month}-${day}`;
 }
 
-function loadReport() {
+gradingDates = 0
+
+function attendance() {
   const xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
+  xhr.onreadystatechange = function () {
     if (xhr.readyState === 4 && xhr.status === 200) {
-      var GRdata = JSON.parse(xhr.responseText);
-      createTable(GRdata);
+      var attendanceData = JSON.parse(xhr.responseText);
+      var totalDates = attendanceData[0].total_unique_dates;
+      var absentDates = attendanceData[0].total_dates_with_skilltag_0;
+      var gradingDates = totalDates - absentDates; // Calculate the gradingDates value
+      console.log('Total Unique Dates:', totalDates);
+      console.log('Total Dates with Skilltag 0:', absentDates);
+      console.log('Total Grading Dates:', gradingDates);
+
+      // Call the loadReport function with the gradingDates value
+      loadReport(gradingDates);
     }
   };
-  xhr.open('GET', "gradingReport.php?start_date="+startDate+"&end_date="+endDate+"&student_id="+studentID, true);
+
+  xhr.open(
+    'GET',
+    'attendance.php?start_date=' + startDate + '&end_date=' + endDate + '&student_id=' + studentID,
+    true
+  );
   xhr.send();
 }
 
-function createTable(array)
-{
-    var table;
-    var grContainer = document.getElementById('grContainer')
+function createTable(array, gradingDates) {
+  var table;
+  var grContainer = document.getElementById('grContainer');
 
-    var categoryContainer = {};
-    var categories = [];
+  var categoryContainer = {};
+  var categories = [];
+  
+  var leftTotal = 0;
+  var rightTotal = 0;
+  var toggle = -1;
 
-    
-        for (let i=0; i<array.length;++i){
-            var skill = array[i][0];
-            var category = array[i][1].trim();
-            var totalGrade = array[i][2];
-            if (!categoryContainer.hasOwnProperty(category)) {
-                // !categoryContainer.hasOwnProperty(category)
-                //Create a new category container if it doesn't exist
-                categoryContainer[category] = document.createElement("div");
-                categories.push(category);
-                table = document.createElement("table");
-                var row = table.insertRow();
-                var cell1 = row.insertCell(0);
-                var cell2 = row.insertCell(1);
-                cell1.innerHTML=category;
-                cell2.innerHTML="Total Grade";
-            }
-            row=table.insertRow();
-            cell1 = row.insertCell(0);
-            cell2 = row.insertCell(1);
-            cell1.innerHTML=skill;
-            cell2.innerHTML=totalGrade;
-            
-            categoryContainer[category].appendChild(table);
+  for (let i = 0; i < array.length; ++i) {
+    var skill = array[i][0];
+    var category = array[i][1].trim();
+    var totalGrade = array[i][2];
+
+    if (!categoryContainer.hasOwnProperty(category)) {
+        
+    if (toggle == 0){
+        
+        cell2.innerHTML +=  ': ' + leftTotal + '/' + rightTotal;
+        leftTotal = 0;
+        rightTotal = 0;
+        
+    }else{
+        toggle = 0;
     }
+      console.log(leftTotal + '/' + rightTotal);
+      categoryContainer[category] = document.createElement('div');
+      categories.push(category);
+      table = document.createElement('table');
+      var row = table.insertRow();
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      cell1.innerHTML = category;
+      cell2.innerHTML = 'Total Grade';
+    }
+
+    row = table.insertRow();
+    var cell1_1 = row.insertCell(0);
+    var cell2_1 = row.insertCell(1);
+    cell1_1.innerHTML = skill;
     
-    //var lastCategoryIndex = categories.length - 1;
-    categories.forEach(function(category, index) {
+    // Calculate the fraction
+    var fraction = totalGrade + '/' + gradingDates;
+    cell2_1.innerHTML = fraction;
+    
+    leftTotal = leftTotal + parseInt(totalGrade);
+    rightTotal = rightTotal + gradingDates; 
+
+    categoryContainer[category].appendChild(table);
+  }
+  
+  cell2.innerHTML +=  ': ' + leftTotal + '/' + rightTotal;
+  leftTotal = 0;
+  rightTotal = 0;
+ 
+  categories.forEach(function (category, index) {
     var categoryDiv = categoryContainer[category];
 
     if (index !== 0) {
-      var categoryHeading = document.createElement("br");
+      var categoryHeading = document.createElement('br');
       categoryDiv.insertBefore(categoryHeading, categoryDiv.firstChild);
     }
 
     grContainer.appendChild(categoryDiv);
   });
+}
+
+function loadReport(gradingDates) {
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var GRdata = JSON.parse(xhr.responseText);
+
+      // Call createTable function with gradingDates as a parameter
+      createTable(GRdata, gradingDates);
+    }
+  };
+  xhr.open(
+    'GET',
+    'gradingReport.php?start_date=' + startDate + '&end_date=' + endDate + '&student_id=' + studentID,
+    true
+  );
+  xhr.send();
 }
 
 $(function() {
@@ -107,7 +160,6 @@ $(function() {
     onSelect: function(selected) {
       var startDate = $(this).datepicker("getDate");
       $("#end-date").datepicker("option", "minDate", startDate);
-      updateDuration();
     }
   });
 
@@ -116,31 +168,9 @@ $(function() {
     onSelect: function(selected) {
       var endDate = $(this).datepicker("getDate");
       $("#start-date").datepicker("option", "maxDate", endDate);
-      updateDuration();
     }
   });
 
-  function updateDuration() {
-    var startDate = $("#start-date").datepicker("getDate");
-    var endDate = $("#end-date").datepicker("getDate");
-    if (startDate && endDate) {
-      var durationInDays = calculateDuration(startDate, endDate);
-      $("#duration").val(durationInDays + " days");
-    }
-  }
-
-  function calculateDuration(startDate, endDate) {
-    var millisecondsPerDay = 24 * 60 * 60 * 1000;
-    var days = Math.round(Math.abs((endDate - startDate) / millisecondsPerDay)) + 1;
-
-    var weekends = 0;
-    for (var i = 0; i < days; i++) {
-      var currentDate = new Date(startDate.getTime() + i * millisecondsPerDay);
-      if (currentDate.getDay() === 0 || currentDate.getDay() === 6) {
-        weekends++;
-      }
-    }
-    var duration = days - weekends;
-    return duration;
-  }
 });
+
+attendance();
