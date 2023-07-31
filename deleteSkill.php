5f3e1d6e744916847cd310af6739c89c92af90a7
anuragraychowdhury <110551628+anuragraychowdhury@users.noreply.github.com>
2023-07-31 10:57:13 -0500

    Update deleteSkill.php

--- deleteSkill.php
+++ deleteSkill.php
@@ -1,9 +1,12 @@
 <?php
 include 'dbConnection.php'; // Assuming you have a database connection file
 
-$skillId = $_POST['skillId'];
+$skillName = $_POST['SkillName']; // Change the parameter name to SkillName
 
-$sql = "DELETE FROM skillKey WHERE skilltag = '$skillId'";
+// Sanitize the input to prevent SQL injection
+$skillName = mysqli_real_escape_string($conn, $skillName);
+
+$sql = "DELETE FROM skillKey WHERE skill = '$skillName'";
 
 if ($conn->query($sql) === TRUE) {
     echo "Skill deleted successfully";
