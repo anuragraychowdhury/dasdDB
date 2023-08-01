e10418bb3b0f828ba12cb80a914a8d1bea3a0f07
anuragraychowdhury <110551628+anuragraychowdhury@users.noreply.github.com>
2023-07-31 19:57:24 -0500

    Update gradingReport.php

--- gradingReport.php
+++ gradingReport.php
@@ -1,13 +1,39 @@
 <?php
 include 'dbConnection.php';
-$startDate = $_GET['start_date'];
-$endDate = $_GET['end_date'];
 $sid = $_GET['student_id'];
 
-//$sql = "SELECT skill, category, SUM(grade) FROM skillKey LEFT JOIN gradingTable on gradingTable.skilltag = skillKey.skilltag WHERE student_id = $sid AND date BETWEEN '$startDate' AND '$endDate' GROUP BY skill, category, grade;";
+// Fetch the marking periods start and end dates from the database
+$sqlMarkingPeriods = "SELECT markingPeriod, MPstartDate, MPendDate FROM mpData";
+$resultMarkingPeriods = $conn->query($sqlMarkingPeriods);
 
-$sql="SELECT skillKey.skill AS skill, skillKey.category, COALESCE(SUM(gradingTable.grade), 0) AS total_grade FROM skillKey LEFT JOIN gradingTable ON gradingTable.skilltag = skillKey.skilltag AND gradingTable.student_id = $sid AND gradingTable.date BETWEEN '$startDate' AND '$endDate' GROUP BY skillKey.skill, skillKey.category ORDER BY skillKey.category;";
+// Array to hold the result data for all marking periods
+$allMarkingPeriodsData = array();
 
-$result = $conn->query($sql);
-echo json_encode($result->fetch_all());
+while ($rowMarkingPeriod = $resultMarkingPeriods->fetch_assoc()) {
+    $markingPeriod = $rowMarkingPeriod['markingPeriod'];
+    $startDate = $rowMarkingPeriod['MPstartDate'];
+    $endDate = $rowMarkingPeriod['MPendDate'];
+
+    // Query to get data for the current marking period
+    $sql = "SELECT skillKey.skill AS skill, skillKey.category, COALESCE(SUM(gradingTable.grade), 0) AS total_grade 
+            FROM skillKey 
+            LEFT JOIN gradingTable ON gradingTable.skilltag = skillKey.skilltag 
+            AND gradingTable.student_id = $sid 
+            AND gradingTable.date BETWEEN '$startDate' AND '$endDate' 
+            GROUP BY skillKey.skill, skillKey.category 
+            ORDER BY skillKey.category";
+
+    $result = $conn->query($sql);
+
+    // Fetch the data for the current marking period and store it in an array
+    $currentMarkingPeriodData = array();
+    while ($row = $result->fetch_assoc()) {
+        $currentMarkingPeriodData[] = $row;
+    }
+
+    // Add the data for the current marking period to the array of all marking periods' data
+    $allMarkingPeriodsData[$markingPeriod] = $currentMarkingPeriodData;
+}
+
+echo json_encode($allMarkingPeriodsData);
 ?>
